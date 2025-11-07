73465c0ad34aac38314bd3da857c15a68edf543e
Nayzeth <sharik.canete@uap.edu.ar>
2025-11-07 03:59:02 -0300

    Fianl

--- Dtos/FlightDto.cs
+++
@@ -1,6 +0,0 @@
-using System; 
-/*namespace PortHub.Api.Dtos
-{
-    public record FlightRequestDto(string FlightNumber, DateTime DepartureTime, DateTime ArrivalTime, long AirlineId);
-    public record FlightResponseDto(long Id, string FlightNumber, DateTime DepartureTime, DateTime ArrivalTime, long AirlineId);
-}*/
